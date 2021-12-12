a9b8fc2f521ebb89fd9e40214cf7ef438185c32e
Tiago Freitas <70216659+FreitsTiago@users.noreply.github.com>
2021-12-12 19:03:32 -0300

    new function

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 █▄ █ :) ██▀▀▀▀▀▀▀╬
 ◥█████◤
 ══╩══╩══
-▬▬▬Tiago Freitas - © 2021 Copyright▬▬▬
+▬▬▬Tiago Freitas - © 2022 Copyright▬▬▬
 */
 
 // Configurando as dependencias
@@ -14,73 +14,12 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const fs = require('fs');
-const crypto = require('crypto')
-const multer = require('multer');
-var cors = require('cors')
 const storege_dir = __dirname + '/data'
 
 // Configurando o express
 app.use(express.json())
 app.use(express.urlencoded())
-app.use(cors())
 
-// Cria as pastas para o funcionamento da aplicação
-if (!fs.existsSync(storege_dir)){
-    fs.mkdirSync(storege_dir);
-}
-if (!fs.existsSync('./tmp')){
-    fs.mkdirSync('./tmp');
-}
-
-// Configurando Multer
-const storege = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, `tmp/`)
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
-})
-
-// Enviar arquivos para o storege
-app.post('/upload', multer({ storage: storege }).array('file'), (req, res) => {
-    if(crypto.createHash('md5').update(req.body.key).digest('hex') != process.env.KEY) { // Verifica a chave de acesso usando hash
-        for(file of req.files){
-            fs.unlink(file.path, () => {})
-        }
-        res.status(202).send('incorrect-key')
-        return
-    }
-
-    for(file of req.files){ // Armazena os arquivos depois de verificar a chave
-        var newPath = ''
-        if(req.body.path == '0'){
-            if(req.body.new_path == null) return
-            if (!fs.existsSync(`${storege_dir}/${req.body.new_path}`)){
-                fs.mkdirSync(`${storege_dir}/${req.body.new_path}`);
-            }
-            newPath = `${storege_dir}/${req.body.new_path}/${file.filename}`
-        } else {
-            newPath = `${storege_dir}/${req.body.path}/${file.filename}`
-        }
-        fs.rename(file.path, newPath, () => {})
-    }
-    res.status(202).send('success')
-});
-
-// Listar as pastas diponiveis em {storege_dir}
-app.get('/list-paths', (req, res) => {
-    fs.readdir(`${storege_dir}`, (err, paths) => {
-        var paths_list = []
-        for(ph of paths){
-            if(fs.lstatSync(`${storege_dir}/${ph}`).isDirectory()){
-                paths_list.push(ph)
-            }
-        }
-        res.json(paths_list)
-    })
-
-})
 
 // Servir os arquivos desponiveis em {storege_dir}
 app.use((req, res) => {
